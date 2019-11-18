57ca1536f17ee7fb7d0f104570b1b41ddc1689e9
Ronald <ronaldbijsma@gmail.com>
2019-11-18 12:13:55 +0100

    "geen zoekterm opgegeven" gefixed
    Signed-off-by: Ronald <ronaldbijsma@gmail.com>

--- search.php
+++ search.php
@@ -90,7 +90,7 @@ include_once("app/model/categorie.php"); // wordt gebruikt voor categorieen opha
 
                 <?php
                 // Check of er een zoekterm is opgegeven in de URL
-                if (isset($_GET['search'])) {
+                if ($_GET['search']!=NULL) {
                     $zoekterm = $_GET['search'];
 
                     //Kijkt hoeveel de opgegeven hoeveelheid zichtbare producten is en maakt er een variabele van.
