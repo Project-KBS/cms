4400a72163170b3f1adc5534288856d90b56b8e7
Ronald <ronaldbijsma@gmail.com>
2019-11-18 14:29:01 +0100

    meer comprimeren
    Signed-off-by: Ronald <ronaldbijsma@gmail.com>

--- search.php
+++ search.php
@@ -144,10 +144,7 @@ include_once("app/model/categorie.php"); // wordt gebruikt voor categorieen opha
                         print("<p>" . $SearchDetails . "</p>");
                         print("<div class='ProductDisplayPrice'>");
                         print("<h5>Prijs: " . $RecommendedRetailPrice . "</h5>");
-                        print("</div>");
-                        print("</div>");
-                        print("</div>");
-                        print("</a>");
+                        print("</div></div></div></a>");
 
                     }
 
