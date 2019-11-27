1283fe04b288db795da71eeec39d59f30cbba905
JobVulkers <55084678+jobvulkers@users.noreply.github.com>
2019-11-27 11:47:06 +0100

    Alle <head> tags worden nu geïnclude ipv 4 keer dezelfde tag
    Signed-off-by: JobVulkers <55084678+JobVulkers@users.noreply.github.com>

--- index.php
+++ index.php
@@ -10,30 +10,11 @@ include_once("app/mediaportal.php");     // wordt gebruikt voor categorie foto's
 <!doctype html>
 <html class="no-js" lang="">
     <head>
-        <meta charset="utf-8">
+        <?php
+        //Hier include je de head-tag-template, alles wat in de header komt pas je aan in "tpl/head-tag-template.php"
+        include("tpl/head-tag-template.php");
 
-        <!-- De beschrijving en eigenschappen van deze pagina -->
-        <title><?php echo VENDOR_NAME ?></title>
-        <meta name="description" content="<?php echo VENDOR_DESCRIPTION ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="theme-color" content="<?php echo VENDOR_THEME_COLOR_PRIMARY?>">
-        <link rel="manifest" href="site.webmanifest">
-        <link rel="apple-touch-icon" href="icon.png">
-
-        <!-- Normalize hebben we nodig voor het schoonmaken van pagina zodat het er in alle browsers hetzelfde uit ziet -->
-        <link rel="stylesheet" type="text/css" href="css/normalize.css">
-        <link rel="stylesheet" type="text/css" href="css/main.css.php">
-        <link rel="stylesheet" type="text/css" href="css/header_footer.css.php">
-
-        <!-- Alle JavaScript dependencies-->
-        <script src="js/vendor/modernizr-3.7.1.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Bootstrap -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        ?>
     </head>
     <body>
         <!-- Onze website werkt niet met Internet Explorer 9 en lager-->
