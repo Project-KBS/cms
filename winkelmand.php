2837e8ebf601616b8005c606494e58e0b668c7f1
hypothermic <admin@hypothermic.nl>
2019-11-28 14:24:32 +0100

    Format winkelmand.php
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- winkelmand.php
+++ winkelmand.php
@@ -10,49 +10,47 @@ include_once("app/cart.php");            // Wordt gebruikt om de huidige test pr
 
 <!doctype html>
 <html class="no-js" lang="">
-<head>
-    <?php
-    //Hier include je de head-tag-template, alles wat in de header komt pas je aan in "tpl/head-tag-template.php"
-    include("tpl/head-tag-template.php");
+    <head>
+        <?php
+            //Hier include je de head-tag-template, alles wat in de header komt pas je aan in "tpl/head-tag-template.php"
+            include("tpl/head-tag-template.php");
+        ?>
+    </head>
+    <body>
 
-    ?>
-</head>
-<body>
-<!-- Onze website werkt niet met Internet Explorer 9 en lager-->
-<!--[if IE]>
-<div id="warning" class="fixed-top"><p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please upgrade your browser to improve your experience and security.</p></div>
-<![endif]-->
+    <!-- Onze website werkt niet met Internet Explorer 9 en lager, laat een waarschuwing zien -->
+    <!--[if IE]>
+        <div id="warning" class="fixed-top"><p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please upgrade your browser to improve your experience and security.</p></div>
+    <![endif]-->
 
-<!-- Hierin  -->
-<div id="pagina-container">
+    <!-- Hierin  -->
+    <div id="pagina-container">
 
-    <!-- Print de header (logo, navigatiebalken, etc.)-->
-    <?php
-    include("tpl/header_template.php");
-    ?>
+        <!-- Print de header (logo, navigatiebalken, etc.)-->
+        <?php
+            include("tpl/header_template.php");
+        ?>
 
-    <!-- Inhoud pagina -->
-    <div class="content-container-home">
+        <!-- Inhoud pagina -->
+        <div class="content-container-home">
 
+            <?php
+                $teller = 0;
+                foreach (Cart::get() as $item => $aantal){
+                    $stmt = (Product::getbyid(Database::getConnection(), $item, 5));
 
-           <?php
-           $teller = 0;
-           foreach (Cart::get() as $item => $aantal){
-               $stmt = (Product::getbyid(Database::getConnection(), $item, 5));
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-               $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    extract($row);
+                        ?>
 
-               extract($row);
-               ?>
+                    <div id="geheel" class="row" style="padding-bottom: 2vh">
 
-               <div id="geheel" class="row" style="padding-bottom: 2vh">
+                            <!-- Links komen de foto's en informatie van het product-->
+                            <div id="Foto" class="col-2">
 
-
-                   <!-- Links komen de foto's en informatie van het product-->
-                   <div id="Foto" class="col-2">
-
-                       <!-- Foto van product of categorie -->
-                       <img src="data:image/png;base64, <?php
+                                <!-- Foto van product of categorie -->
+                                <img src="data:image/png;base64, <?php
                                                        if (isset($Photo) && $Photo != null) {
                                                            print($Photo);
                                                        } else {
@@ -60,100 +58,105 @@ include_once("app/cart.php");            // Wordt gebruikt om de huidige test pr
                                                        }
                                                         ?>" id="Productphoto" class="Productphoto" >
 
-                   </div>
+                            </div>
 
-                   <div id="Omschrijving" class="col-4">
+                            <div id="Omschrijving" class="col-4">
 
-                       <?php if(isset($StockItemName) && $StockItemName != null){
-                           print($StockItemName);
-                       }
-                       print("<br>");
-                       if(isset($MarketingComments) && $MarketingComments != null){
-                           print($MarketingComments);
-                       }
-                       ?>
+                                <?php
+                                    if (isset($StockItemName) && $StockItemName != null) {
+                                        print($StockItemName);
+                                    }
 
-                   </div>
+                                    print("<br>");
 
-                   <div class="col-1">
-                       <input style="width: 100%" type="number" id="test<?php print($teller); ?>" value="<?php print($aantal); ?>" min="0">
-                   </div>
+                                    if (isset($MarketingComments) && $MarketingComments != null) {
+                                        print($MarketingComments);
+                                    }
+                                ?>
 
-                   <!-- hier komen de aantallen en totaalprijzen-->
-                   <div id="Prijs" class="col-5">
+                            </div>
 
-                      <p id="prijs<?php print($teller); ?>">Totaalprijs: € <?php print( round($RecommendedRetailPrice * (1+ $TaxRate/100),2)); ?></p>
+                            <div id="Aantal" class="col-1">
+                                <input style="width: 100%" type="number" id="test<?php print($teller); ?>" value="<?php print($aantal); ?>" min="0">
+                            </div>
 
+                            <!-- hier komen de aantallen en totaalprijzen-->
+                            <div id="Prijs" class="col-5">
 
-                       <!-- Zorgt ervoor dat je geen negatief getal kan invullen-->
-                       <script>
-                           // Dit is het input veld
-                           const hoeveelheid_input<?php print($teller); ?> = document.getElementById('test<?php print($teller); ?>');
-
-                           // Listen for input event on numInput. ( blokkeert negatieve getallen)
-                           hoeveelheid_input<?php print($teller); ?>.onkeydown = function(e) {
-                               if(!((e.keyCode > 95 && e.keyCode < 106)
-                                   || (e.keyCode > 47 && e.keyCode < 58)
-                                   || e.keyCode === 8)) {
-                                   return false;
-                               }
-                           }
-
-                           //
-                           const totaalprijs<?php print($teller); ?> = document.getElementById(('prijs<?php print($teller); ?>'));
-
-                           hoeveelheid_input<?php print($teller); ?>.onchange = function () {
-                               totaalprijs<?php print($teller); ?>.innerHTML = "Totaalprijs: €"+(hoeveelheid_input<?php print($teller); ?>.value *
-                                                                            <?php print($RecommendedRetailPrice * (1+ $TaxRate/100)) ?>).toFixed(2);
-                           }
-
-                       </script>
+                                <p id="prijs<?php print($teller); ?>">Totaalprijs: € <?php print( round($RecommendedRetailPrice * (1+ $TaxRate/100),2)); ?></p>
 
 
+                                <!-- Zorgt ervoor dat je geen negatief getal kan invullen-->
+                                <script>
+                                    // Dit is het input veld
+                                    const hoeveelheid_input<?php print($teller); ?> = document.getElementById('test<?php print($teller); ?>');
 
-                   </div>
+                                    // Listen for input event on numInput. ( blokkeert negatieve getallen)
+                                    hoeveelheid_input<?php print($teller); ?>.onkeydown = function(e) {
+                                        if(!((e.keyCode > 95 && e.keyCode < 106)
+                                            || (e.keyCode > 47 && e.keyCode < 58)
+                                            || e.keyCode === 8)) {
+                                            return false;
+                                        }
+                                    };
 
+                                    // Dit is de <p> tag van totaalprijs
+                                    const totaalprijs<?php print($teller); ?> = document.getElementById(('prijs<?php print($teller); ?>'));
 
-               </div>
+                                    hoeveelheid_input<?php print($teller); ?>.onchange = function () {
+                                        totaalprijs<?php print($teller); ?>.innerHTML = "Totaalprijs: €" +
+                                            (hoeveelheid_input<?php print($teller); ?>.value * <?php print($RecommendedRetailPrice * (1+ $TaxRate/100)) ?>).toFixed(2);
+                                    }
 
-           <?php $teller++; } ?>
+                                </script>
 
-        <div class="row">
-        <div id="opvul" class="col-6">
-        </div>
+                            </div>
 
-           <!-- Bestelknop komt hier met foto's van betalmethoden-->
-           <div id="Totaal" class="col-4">
+                        </div>
 
-               <!-- Prijzen totaal -->
-               <p>Exclusief btw:</p>
-               <p>Inclusief btw:</p>
-               <p>Totaalprijs: </p>
+                        <?php
 
+                    $teller++;
+                }
 
+                ?>
 
+            <div class="row">
 
+                <div id="opvul" class="col-7">
 
-           </div>
-            <div id="verstuur" class="col-2">
+                </div>
 
-                <!-- Submit knop en betaalmethoden afbeeldingen -->
-                <input type="submit" value="Verder naar bestellen">
+                <!-- Bestelknop komt hier met foto's van betalmethoden-->
+                <div id="Totaal" class="col-3">
 
-                <img src="img/logo/ideal.png" style="max-width: 60%">
+                    <!-- Prijzen totaal -->
+                    <p id="prijs-excl">Exclusief btw:</p>
+                    <p id="prijs-incl">Inclusief btw: fuck</p>
+                    <p id="prijs-totaal">Totaalprijs: </p>
+
+                </div>
+
+                <div id="verstuur" class="col-2">
+
+                    <!-- Submit knop en betaalmethoden afbeeldingen -->
+                    <input type="submit" value="Verder naar bestellen">
+
+                    <img src="img/logo/ideal.png" style="max-width: 60%">
+                </div>
+
             </div>
+
         </div>
 
     </div>
 
-</div>
+    <div class="footer-container">
+        <?php
+            // Dit is de footer
+            include("tpl/footer_template.php");
+        ?>
+    </div>
 
-<div class="footer-container">
-    <?php
-    include("tpl/footer_template.php");
-
-    ?>
-
-</div>
-</body>
+    </body>
 </html>
