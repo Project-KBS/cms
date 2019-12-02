8c2a0ab6ac29dec11597668d04da80142b2aec62
Ronald <ronaldbijsma@gmail.com>
2019-12-02 13:26:45 +0100

    ideal-testomgeving gecomment
    Signed-off-by: Ronald <ronaldbijsma@gmail.com>

--- ideal-testomgeving.php
+++ ideal-testomgeving.php
@@ -6,20 +6,21 @@ include_once("mollie-api-php/vendor/autoload.php");
 
 // de mollie API activeren en een key zetten.
 $mollie = new \Mollie\Api\MollieApiClient();
-$mollie->setApiKey("test_3mSHuyjnfdsnyBFRKv6P7ucgqPh4Tc");
+$mollie->setApiKey("test_3mSHuyjnfdsnyBFRKv6P7ucgqPh4Tc"); // deze key zorgt ervoor dat de betaling wordt gekoppeld aan ons account bij Mollie, zodat we daar de betalingen kunnen zien en de betaalmethodes aanpassen.
 
 // een betaling aanmaken
 $payment = $mollie->payments->create([
     "amount" => [
-        "currency" => "EUR",
-        "value" => "10.00"
+        "currency" => "EUR", //deze waarde zorgt voor het type valuta van de betaling
+        "value" => "10.00"// deze waarde is het totaal inclusief BTW worden
     ],
-    "description" => "Wide World Importers",
-    "redirectUrl" => "http://localhost/confirm-order.php",
-    "webhookUrl"  => "https://webshop.example.org/mollie-webhook/",
+    "description" => "Wide World Importers", // dit is de beschrijving van de betaling bij het bankafschrift van de klant
+    "redirectUrl" => "http://localhost/confirm-order.php", // dit is de locatie waar Mollie de klant heenstuurt na de betaling
+    "webhookUrl"  => "https://webshop.example.org/mollie-webhook/", // geen flauw idee wat dit is
 ]);
 
 //nu moeten we het payment id opslaan om later te controleren of er betaald is
+
 
 //de gebruiker wordt doorgestuurd naar een betalingspagina
 header("Location: " . $payment->getCheckoutUrl(), true, 303);
