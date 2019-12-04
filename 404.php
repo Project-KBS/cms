2c298141c35e7efb4642198e9128fcb05159d997
JobVulkers <55084678+jobvulkers@users.noreply.github.com>
2019-12-04 15:18:50 +0100

    404 pagina start nu ook een session
    Signed-off-by: JobVulkers <55084678+JobVulkers@users.noreply.github.com>

--- 404.php
+++ 404.php
@@ -2,36 +2,19 @@
 // Uit deze php bestanden gebruiken wij functies of variabelen:
 include_once("app/vendor.php");          // wordt gebruikt voor website beschrijving
 include_once("app/database.php");        // wordt gebruikt voor database connectie
-include_once("app/model/categorie.php"); // wordt gebruikt voor categorieen ophalen uit DB
+include_once("app/cart.php");        // wordt gebruikt voor database connectie
+include_once("app/model/categorie.php"); // wordt gebruikt voor categorieen ophalen uit DB;
+
+
 ?>
 
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
-        <meta charset="utf-8">
+        <?php
+            include("tpl/head-tag-template.php");
 
-        <!-- De beschrijving en eigenschappen van deze pagina -->
-        <title><?php echo VENDOR_NAME ?></title>
-        <meta name="description" content="<?php echo VENDOR_DESCRIPTION ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="theme-color" content="<?php echo VENDOR_THEME_COLOR_PRIMARY ?>">
-        <link rel="manifest" href="site.webmanifest">
-        <link rel="apple-touch-icon" href="icon.png">
-
-        <!-- Normalize hebben we nodig voor het schoonmaken van pagina zodat het er in alle browsers hetzelfde uit ziet -->
-        <link rel="stylesheet" type="text/css" href="css/normalize.css">
-        <link rel="stylesheet" type="text/css" href="css/main.css.php">
-        <link rel="stylesheet" type="text/css" href="css/header_footer.css.php">
-
-        <!-- Alle JavaScript dependencies-->
-        <script src="js/vendor/modernizr-3.7.1.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Bootstrap -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        ?>
     </head>
     <body>
 
