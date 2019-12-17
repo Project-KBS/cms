5fb68ad34293c754cd84ac495327f172b9673885
Ronald <ronaldbijsma@gmail.com>
2019-12-17 13:47:44 +0100

    reviews aanpassen werkt
    Signed-off-by: Ronald <ronaldbijsma@gmail.com>

--- edit-review.php
+++ edit-review.php
@@ -8,7 +8,6 @@ include_once("app/model/categorie.php"); // wordt gebruikt voor categorieen opha
 include_once("app/model/product.php"); // wordt gebruikt om informatie uit de database te halen
 include_once("app/model/review.php"); //wordt gebruikt om de review class te includen
 
-$email = Authentication::getEmail();
 // Deze pagina vereist een GET parameter: "id" met integer value van het product.
 // Als deze param niet meegegeven is sturen we de user terug naar index.php
 if (!isset($_GET["id"]) || filter_var($_GET["id"], FILTER_VALIDATE_INT) == false) {
@@ -45,20 +44,25 @@ if (!isset($_GET["id"]) || filter_var($_GET["id"], FILTER_VALIDATE_INT) == false
 
             <!-- Inhoud van de pagina -->
             <div class="content-container" style="margin-top: 2.75vw;">
-
             <?php
-            $stmt = Review::readOne(Database::getConnection(), $_GET['id'], $email);
+            $title=0;
+            $score=0;
+            $description=0;
+            $stmt = Review::readOne(Database::getConnection(), $_GET['id'], Authentication::getEmail());
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 extract($row);
-                ?>
-                <form action="product.php?id=<?php print($_GET["id"]); ?>" id="reviews" method="post"
-                      style="display:none; width: 50vw%; margin-left: 20%">
+                $title=$Title;
+                $score=$Score;
+                $description=$Description;
+            }
+            ?>
+                <form action="product.php?id=<?php print($_GET["id"]); ?>" id="reviews" method="post">
 
                     <input type="text"
                            name="title"
                            class="reviewInputs"
                            placeholder="Titel van je review"
-                           value="<?php print($Title);?>"
+                           value="<?php print($title);?>"
                     />
                     <br>
 
@@ -67,7 +71,7 @@ if (!isset($_GET["id"]) || filter_var($_GET["id"], FILTER_VALIDATE_INT) == false
 
                         for ($i = 1; $i <= 10; $i++) {
                             print("<option value='$i' ");
-                            if($i==$Score){
+                            if($i==$score){
                                 print("selected='true'");
                             }
                             print(">$i</option>");
@@ -79,7 +83,7 @@ if (!isset($_GET["id"]) || filter_var($_GET["id"], FILTER_VALIDATE_INT) == false
                     <textarea name="reviewInputs"
                               class="reviewInputs"
                               placeholder="Schrijf hier je review"
-                              style="height:20vw;"><?php print($Description);?></textarea>
+                              style="height:20vw;"><?php print($description);?></textarea>
 
                     <br>
 
@@ -91,9 +95,6 @@ if (!isset($_GET["id"]) || filter_var($_GET["id"], FILTER_VALIDATE_INT) == false
 
                     <br>
                 </form>
-                <?php
-            }
-            ?>
             </div>
         </div>
         <div class="footer-container">
